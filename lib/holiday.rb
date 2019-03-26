83c084064455c4f19cf5897962d035a132da2d9a
David zhang <d.ydzdavid@yahoo.com>
2019-03-26 04:34:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,7 +70,12 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.to_s.capitalize!}:"
     holiday.each do |day, supply|
-      
+      new_holiday = holiday.to_s.split("_").collect do |word|
+        words = word.capitalize!
+      end.join(" ")
+
+      puts "  #{split_holiday}: #{supplies.join(", ")}" 
+    end 
     end
   end
 end
